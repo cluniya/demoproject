c324a1930e07a65751b2eefe82e781242ef60c53
cluniya <chandanluniya1986@gmail.com>
2024-06-06 11:34:39 +0530

    Doing Network calls and andling response the right way

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,56 @@
 // import { Provider } from 'react-redux';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { useEffect } from 'react';
+import { uiActions } from './Stores/ui-slice';
+import Notification from './components/UI/Notification';
 // import store from './Stores/store';
-
+let isInitial = true;
 function App() {
   const CartisOpen = useSelector(state => state.ui.cartIsVisible)
+  const cart = useSelector(state => state.cart);
+  const notification = useSelector(state => state.ui.notification)
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    const sendCartData = async ()=>{
+
+      dispatch(uiActions.setNotification({
+        status:'pending',
+        title: 'sendinf...',
+        message : 'sending cart data...'
+      }))
+      const response = await fetch('https://ecommerce-http-513d4-default-rtdb.firebaseio.com/cart.json',{
+      method:'PUT',
+      body:JSON.stringify(cart),
+    })
+    if (!response.ok) {
+      throw new error('sending cart data failder');
+    }
+    // const responseData = await response.json();
+    dispatch(uiActions.setNotification({
+      status:'success',
+      title: 'seccess...',
+      message : 'send cart data successfully!'
+    }))
+    }
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    sendCartData().catch(error => {
+      dispatch(uiActions.setNotification({
+        status:'error',
+        title: 'Error!',
+        message : 'sending cart data Failed!'
+      }))
+    })
+  },[cart,dispatch])
   return (
       <Layout >
+        {notification && <Notification status={notification.status} title={notification.title} message={notification.message} />}
       {CartisOpen && <Cart />}
       <Products />
     </Layout>
--- src/components/Shop/Products.jsx
+++ src/components/Shop/Products.jsx
@@ -1,11 +1,19 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
-const DUMMY = [
-  { id: 'p1', price: 6, title: 'my first book', des: 'i have written' },
-  { id: 'p2', price: 7, title: 'my second book', des: 'i have written' },
-  { id: 'p3', price: 8, title: 'my third book', des: 'i have written' },
-  { id: 'p4', price: 9, title: 'my fourth book', des: 'i have written' }
+const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    price: 6,
+    title: 'My First Book',
+    description: 'The first book I ever wrote',
+  },
+  {
+    id: 'p2',
+    price: 5,
+    title: 'My Second Book',
+    description: 'The second book I ever wrote',
+  },
 ];
 
 const Products = (props) => {
@@ -13,12 +21,13 @@ const Products = (props) => {
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        {DUMMY.map((product) => (
+        {DUMMY_PRODUCTS.map((product) => (
           <ProductItem
             key={product.id}
+            id={product.id}
             title={product.title}
             price={product.price}
-            description={product.des}
+            description={product.description}
           />
         ))}
       </ul>
