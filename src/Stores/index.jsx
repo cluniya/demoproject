a8812ad01c64920f04d80dfb70f2e7d5bd82a257
cluniya <chandanluniya1986@gmail.com>
2024-06-06 06:48:54 +0530

    Shoping page app using reducers

--- src/Stores/index.jsx
+++ src/Stores/index.jsx
@@ -1,37 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import uiSlice from "./ui-slice";
+import cartSlice from "./cart-slice";
 const store = configureStore({
-    reducer: {ui : uiSlice.reducer}
+    reducer: {ui : uiSlice.reducer,cart:cartSlice.reducer}
+
+
 })
 
 export default store;
 
-// import { configureStore } from "@reduxjs/toolkit";
-// import { createSlice } from "@reduxjs/toolkit";
-
-// // Define the slice
-// const cartSlice = createSlice({
-//     name: 'cart',
-//     initialState: { cartIsOpen: false },
-//     reducers: {
-//         cartToggle(state) {
-//             state.cartIsOpen = !state.cartIsOpen;
-//         }
-//     }
-// });
-
-// // Export the action
-// export const { cartToggle } = cartSlice.actions;
-
-// // Create and export the reducer
-// const cartReducer = cartSlice.reducer;
-
-// // Create the store
-// const store = configureStore({
-//     reducer: {
-//         cart: cartReducer
-//     }
-// });
-
-// // Export the store
-// export default store;
